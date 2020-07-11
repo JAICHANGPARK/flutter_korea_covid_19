bb32c6c047a125c33e1ad82a2ff138241f5441a7
Dreamwalker <aristojeff@gmail.com>
2020-07-11 18:21:34 +0900

    Update under_construct_page.dart

--- lib/ui/common/under_construct_page.dart
+++ lib/ui/common/under_construct_page.dart
@@ -1,6 +1,37 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 
-class UnderConstructPage extends StatelessWidget {
+class UnderConstructPage extends StatefulWidget {
+  @override
+  _UnderConstructPageState createState() => _UnderConstructPageState();
+}
+
+class _UnderConstructPageState extends State<UnderConstructPage> {
+  Future<String> getTextFromRemoteConfig() async {
+    final RemoteConfig remoteConfig = await RemoteConfig.instance;
+    final defaults = <String, dynamic>{'welcome': 'default welcome'};
+    await remoteConfig.setDefaults(defaults);
+    await remoteConfig.fetch(expiration: const Duration(seconds: 10));
+    await remoteConfig.activateFetched();
+    var tmp = remoteConfig.getString("under_construct_text");
+//    return tmp == "0" ? false : true;
+    return tmp;
+  }
+
+  String _text;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getTextFromRemoteConfig().then((value) {
+
+      setState(() {
+        _text = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,12 +43,15 @@ class UnderConstructPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Image.network(
-              "https://assets-ouch.icons8.com/thumb/89/66578613-b95f-46f4-b047-87cea9a478d8.png",
-          width: MediaQuery.of(context).size.width / 1.5,),
+
+//            "https://assets-ouch.icons8.com/thumb/89/66578613-b95f-46f4-b047-87cea9a478d8.png",
+            "https://ouch-cdn.icons8.com/preview/836/7d198965-5de4-4bd4-8268-89f409b6a406.png",
+            width: MediaQuery.of(context).size.width / 1.5,
+          ),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Center(
-              child: Text("....잠시만 기다려주세요"),
+              child: Text(_text ?? "...잠시만 기다려주세요..."),
             ),
           ),
         ],
